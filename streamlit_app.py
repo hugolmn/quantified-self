a945623a1e25a7dc5a83fa9ef989982d17329f01
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-05-29 17:04:29 +0200

    Added file upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title("Google Fit data analysis")
+
+
+uploaded_files = st.file_uploader("Upload CSV files", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
