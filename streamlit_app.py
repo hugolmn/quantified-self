5f7bed9ac970a5cb9ff4ddd9cc916f7e5efad32e
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-06-18 17:56:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,2 @@
 import streamlit as st
 import pandas as pd
-
-st.title("Google Fit data analysis")
-
-df = pd.DataFrame()
-
-uploaded_files = st.file_uploader("Upload CSV files", accept_multiple_files=True)
-if uploaded_files:
-     df = pd.concat([
-         pd.read_csv(file).assign(start_date=file.name.split('.')[0])
-         for file in uploaded_files
-     ]) 
-     st.dataframe(df)
