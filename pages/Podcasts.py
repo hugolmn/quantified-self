edbc59470b56055d8dd417209c63d9c3dac6232c
Hugo Le Moine <39267118+hugolmn@users.noreply.github.com>
2022-06-18 18:12:12 +0200

    Read podcasts data from google sheets

--- pages/Podcasts.py
+++ pages/Podcasts.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from gsheetsdb import connect
+import pandas as pd
 
 st.title("Podcasts")
+
+# Create a connection object.
+con = connect()
+
+sheet_podcasts = st.secrets['podcasts']
+df_podcasts = pd.read_sql_query(f'SELECT * FROM {sheet_podcasts}', con)
+
+st.dataframe(df_podcasts)
