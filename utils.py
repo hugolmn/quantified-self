f53c2cbf136a074b690a4ff3b1733d0f2af25877
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-08-27 22:04:34 +0200

    Added utils functions

---
+++ utils.py
@@ -0,0 +1,62 @@
+import io
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
+import streamlit as st
+
+scope = ['https://www.googleapis.com/auth/drive.readonly']
+# credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
+
+
+def find_file_id(q=""):
+    service = build('drive', 'v3', credentials=credentials)
+    files = []
+    page_token = None
+    while True:
+        # pylint: disable=maybe-no-member
+        response = service.files().list(q=q,
+                                        spaces='drive',
+                                        fields='nextPageToken, '
+                                            'files(id, name)',
+                                        pageToken=page_token).execute()
+        for file in response.get('files', []):
+            # Process change
+            print(f'Found file: {file.get("name")}, {file.get("id")}')
+        files.extend(response.get('files', []))
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+    return files
+
+def download_file(file_id):
+    """Downloads a file
+    Args:
+        real_file_id: ID of the file to download
+    Returns : IO object with location.
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=credentials)
+
+        # pylint: disable=maybe-no-member
+        request = service.files().get_media(fileId=file_id)
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(F'Download {int(status.progress() * 100)}.')
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+
+    return file
