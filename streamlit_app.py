b3decc3c4b4ab9e06c8eacc81c2257d16cbe8b0f
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-09-04 21:51:48 +0200

    Main page more detailed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,27 @@
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(
+     page_title="Quantified Self",
+    #  layout="wide",
+    #  initial_sidebar_state="expanded",
+     menu_items={
+         'Get help': 'https://twitter.com/hugo_le_moine_',
+         'Report a Bug': 'https://twitter.com/hugo_le_moine_',
+         'About': "# Quantified Self. This is an app still under construction!"
+     }
+ )
+
+st.title('Quantified Self')
+st.markdown('This project aims at collecting, aggegating and reporting my personal data.')
+
+st.markdown("""
+    ### Data sources include:
+    - <a href="/Anki" target="_self">AnkiDroid</a> : an app to learn vocabulary through flashcards.
+    - Spotify : listening history.
+    - Garmin : biometric data collected through my watch.
+    - <a href="/Podcasts" target="_self">Podcast Addict</a> : a podcast player I use since 2017.
+    - Zero : an intermittent fasting tracker.
+    """,
+    unsafe_allow_html=True
+    )
