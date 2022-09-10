974cf2fa6f82fe91e84e5eb382b0e4079794a1a9
Hugo Le Moine <hugo.le-moine@outlook.fr>
2022-09-10 19:52:15 +0200

    Fixed cert download if not existing

--- pages/CockroachDB.py
+++ pages/CockroachDB.py
@@ -3,8 +3,12 @@ import streamlit as st
 import pandas as pd
 from utils import get_cockroachdb_conn
 
-if not os.path.exists('$HOME/.postgresql/root.crt'):
-    os.system(f"curl --create-dirs -o $HOME/.postgresql/root.crt -O {st.secrets['get_certificate_cockroachdb']}")
+certificate_path = os.path.expanduser('~/.postgresql/root.crt')
+if not os.path.exists(certificate_path):
+    os.system(f"curl --create-dirs -o ~/.postgresql/root.crt -O {st.secrets['get_certificate_cockroachdb']}")
+
+import glob
+glob.glob('$HOME')
 
 conn = get_cockroachdb_conn('garmin')
 df = pd.read_sql('SELECT * FROM stress', conn)
