2774e97e167787f79a38e044549b3621342a30c2
Hugo Le Moine <hugo.le-moine@outlook.fr>
2023-06-11 21:24:09 +0200

    Fixed datetime issue in groupby + graph improvements

--- pages/Activity.py
+++ pages/Activity.py
@@ -21,7 +21,7 @@ steps_df = get_steps_detailed_data().copy()
 
 def generate_metric_steps(col, label, df, days=None):
     if days:
-        steps = df[df.date > df.date.max() - pd.Timedelta(f'{days}D')].steps.sum() / days
+        steps = df[df.date > (df.date.max() - pd.Timedelta(f'{days}D'))].steps.sum() / days
     else:
         steps = df.steps.sum() / df.date.dt.date.nunique()
 
@@ -40,30 +40,23 @@ steps_metrics = {
 for params, col in zip(steps_metrics.items(), st.columns(4)):
     generate_metric_steps(col, params[0], steps_df, params[1])
 
-# Steps plots
-col1, col2 = st.columns(2)
-
-# 7 day rolling average + scatterplot
+# 7 day rolling average
 daily_steps = (steps_df
-    .groupby(steps_df['date'].dt.date, group_keys=False)
+    .groupby(steps_df['date'].dt.date)
+    .steps
     .sum()
     .reset_index()
 )
 daily_steps['date'] = pd.to_datetime(daily_steps.date)
 
-steps_scatterplot_daily = alt.Chart(daily_steps).mark_bar(
+steps_barplot_daily = alt.Chart(daily_steps).mark_bar(
     color=st.secrets["theme"]['primaryColor'],
     clip=True
 ).encode(
     x=alt.X(
         'yearmonthdate(date):T',
-        title='Date',
-        # scale=alt.Scale(
-        #     domain=[
-        #         daily_steps['date'].iloc[-90].isoformat(),
-        #         daily_steps['date'].iloc[-1].isoformat(),
-        #     ]
-        # )
+        title='',
+        axis=alt.Axis(format='%Y', tickCount='year', grid=True)
     ),
     y=alt.Y(
         'steps:Q',
@@ -75,34 +68,37 @@ steps_scatterplot_daily = alt.Chart(daily_steps).mark_bar(
 steps_weekly_rolling_mean_plot = alt.Chart(
     daily_steps.assign(legend='30-day average')
 ).mark_line(
+    interpolate="basis",
     color=st.secrets["theme"]['secondaryColor']
 ).transform_window(
     rolling_mean='mean(steps)',
-    frame=[-15, -15]
+    frame=[-15, 15]
 ).encode(
     x=alt.X('yearmonthdate(date):T'),
     y=alt.Y('rolling_mean:Q', title=''),
     color=alt.Color(
         'legend',
-        title='',
+        title='Trend Line',
         scale=alt.Scale(
             range=[st.secrets["theme"]['secondaryColor']]*2
         ),
-        legend=alt.Legend(orient='top-right')
+        legend=alt.Legend(orient='top')
     )
 )
 
-col1.altair_chart(
-    steps_scatterplot_daily + steps_weekly_rolling_mean_plot,
+st.altair_chart(
+    steps_barplot_daily + steps_weekly_rolling_mean_plot,
     use_container_width=True,
     theme='streamlit'
 )
 
-# st.write((steps_scatterplot_daily + steps_weekly_rolling_mean_plot).to_dict())
-
 # Hourly plot
-steps_scatterplot_hourly = alt.Chart(steps_df).mark_line().encode(
-    x=alt.Y('hoursminutes(date):T', title='Time'),
+steps_scatterplot_hourly = alt.Chart(steps_df).mark_line(interpolate="basis").encode(
+    x=alt.Y(
+        'hoursminutes(date):T',
+        title='',
+        axis=alt.Axis(format='%H:%M', tickCount='hour', grid=True)
+    ),
     y=alt.Y(
         'mean(steps):Q',
         title='Hourly Steps',
@@ -116,10 +112,11 @@ steps_scatterplot_hourly = alt.Chart(steps_df).mark_line().encode(
                 st.secrets["theme"]['secondaryColor']
             ]
         ),
+        legend=alt.Legend(orient='top')
     ),
     tooltip=['hoursminutes(date)']
 )
-col2.altair_chart(
+st.altair_chart(
     steps_scatterplot_hourly,
     use_container_width=True,
     theme='streamlit'
@@ -133,7 +130,11 @@ col1, col2 = st.columns(2)
 activity_level_chart = alt.Chart(steps_df.dropna(subset=['activity_level'])).mark_bar().encode(
     x=alt.X('yearmonthdate(date)', title='Date'),
     y=alt.Y('count(activity_level)', stack='normalize', title='Percentage of day'),
-    color=alt.Color('activity_level:N', title='Activity level'),
+    color=alt.Color(
+        'activity_level:N',
+        title='Activity level',
+        legend=alt.Legend(orient='top')
+    ),
     tooltip=[
         alt.Tooltip('yearmonthdate(date)', title='Date'),
         alt.Tooltip('activity_level', title='Activity level'),
@@ -146,7 +147,11 @@ col1.altair_chart(activity_level_chart, use_container_width=True, theme='streaml
 sedentarity_chart = alt.Chart(steps_df[steps_df.activity_level.isin(['active', 'highlyActive'])]).mark_bar().encode(
     x=alt.X('yearmonthdate(date)', title='Date'),
     y=alt.Y('count(activity_level)', title='Percentageof day'),
-    color=alt.Color('activity_level:N', title='Activity level'),
+    color=alt.Color(
+        'activity_level:N',
+        title='Activity level',
+        legend=alt.Legend(orient='top')
+    ),
     tooltip=[
         alt.Tooltip('yearmonthdate(date)', title='Date'),
         alt.Tooltip('activity_level', title='Activity level'),
