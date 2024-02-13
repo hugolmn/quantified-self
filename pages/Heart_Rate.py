9e81e3d2c256a2eaa101d8ff3bd1e0397ce38544
Hugo Le Moine <hugo.le-moine@outlook.fr>
2024-02-13 08:56:08 +0100

    Improved Hear rate page

--- pages/Heart_Rate.py
+++ pages/Heart_Rate.py
@@ -3,19 +3,20 @@ Heart rate page.
 """
 import streamlit as st
 import altair as alt
-import numpy as np
-import seaborn as sns
 from utils import load_css, get_garmin_data
+
 st.set_page_config(layout="wide")
 load_css()
 
-st.title('Heart Rate')
+st.title("Heart Rate")
 
 # Resting heart rate charts
-st.header(f'Resting Heart Rate (RHR)')
+st.header("Resting Heart Rate (RHR)")
 # Get RHR data
 rhr_df = get_garmin_data("""SELECT date, resting_heart_rate FROM stats""").copy()
-st.write(f'{len(rhr_df)} days of data collected. RHR is calculated using the lowest 30 minute average in a 24 hour period.')
+st.write(
+    f"{len(rhr_df)} days of data collected. RHR is calculated using the lowest 30 minute average in a 24 hour period."  # noqa: E501
+)
 
 # Calculate metrics: weekly, monthly, quarterly and all time RHR mean.
 weekly_rhr = rhr_df.resting_heart_rate.iloc[-7:].mean()
@@ -29,85 +30,96 @@ col1.metric(
     "Weekly RHR",
     f"{weekly_rhr:.1f} bpm",
     delta=f"{weekly_rhr-all_time_rhr:.1f} bpm vs all time",
-    delta_color='inverse',
+    delta_color="inverse",
 )
 col2.metric(
     "Monthly RHR",
     f"{monthly_rhr:.1f} bpm",
     delta=f"{monthly_rhr-all_time_rhr:.1f} bpm vs all time",
-    delta_color='inverse'
+    delta_color="inverse",
 )
 col3.metric(
     "Quarterly RHR",
     f"{quarterly_rhr:.1f} bpm",
     delta=f"{quarterly_rhr-all_time_rhr:.1f} bpm vs all time",
-    delta_color='inverse'
+    delta_color="inverse",
 )
-col4.metric(
-    "All Time RHR",
-    f"{all_time_rhr:.1f} bpm",
-    delta=f"{all_time_rhr-all_time_rhr:.1f} bpm vs all time",
-    delta_color='inverse'
-)
+col4.metric("All Time RHR", f"{all_time_rhr:.1f} bpm", delta_color="inverse")
 
 # Scatterplot + weekly and monthly rolling means
-rhr_scatterplot = alt.Chart(rhr_df).mark_point(color='#FFFFFF', size=2).encode(
-    x=alt.Y(
-        'yearmonthdate(date):T',
-        title='',
-        axis=alt.Axis(
-            format='%m-%Y',
-            tickCount='month',
-            grid=True
-        )
-    ),
-    y=alt.Y(
-        'resting_heart_rate:Q',
-        title='Resting Heart Rate',
-        scale=alt.Scale(zero=False)
-    ),
-    size=alt.Size(scale=alt.Scale(range=[100, 100])),
-    tooltip=[
-        alt.Tooltip('date', title='Date'),
-        alt.Tooltip('resting_heart_rate', title='RHR')
-    ]
-).interactive(bind_y=False)
-
-rhr_df['Weekly Mean'] = rhr_df.resting_heart_rate.rolling(7, min_periods=0, center=True).mean()
-rhr_df['Monthly Mean'] = rhr_df.resting_heart_rate.rolling(30, min_periods=0, center=True).mean()
-
-rolling_mean_chart = alt.Chart(
-    rhr_df.drop(columns='resting_heart_rate')
-          .melt('date', var_name='rolling', value_name='resting_heart_rate')
-).mark_line(color='#3B97F3').encode(
-    x=alt.X('date', title=''),
-    y=alt.Y('resting_heart_rate'),
-    color=alt.Color(
-        'rolling:N',
-        title='',
-        scale=alt.Scale(
-            range=[
-                st.secrets["theme"]['primaryColor'],
-                st.secrets["theme"]['secondaryColor']
-            ]
+rhr_scatterplot = (
+    alt.Chart(rhr_df)
+    .mark_point(color="#FFFFFF", size=2)
+    .encode(
+        x=alt.Y(
+            "yearmonthdate(date):T",
+            title="",
+            axis=alt.Axis(format="%m-%Y", tickCount="month", grid=True),
         ),
-        legend=alt.Legend(
-            orient='top',
-        )
-    ),
+        y=alt.Y(
+            "resting_heart_rate:Q",
+            title="Resting Heart Rate",
+            scale=alt.Scale(zero=False),
+        ),
+        size=alt.Size(scale=alt.Scale(range=[100, 100])),
+        tooltip=[
+            alt.Tooltip("date", title="Date"),
+            alt.Tooltip("resting_heart_rate", title="RHR"),
+        ],
+    )
+    .interactive(bind_y=False)
 )
-st.altair_chart(rhr_scatterplot + rolling_mean_chart, use_container_width=True, theme='streamlit')
+
+rhr_df["Weekly Mean"] = rhr_df.resting_heart_rate.rolling(
+    7, min_periods=0, center=True
+).mean()
+rhr_df["Monthly Mean"] = rhr_df.resting_heart_rate.rolling(
+    30, min_periods=0, center=True
+).mean()
+
+rolling_mean_chart = (
+    alt.Chart(
+        rhr_df.drop(columns="resting_heart_rate").melt(
+            "date", var_name="rolling", value_name="resting_heart_rate"
+        )
+    )
+    .mark_line(color="#3B97F3")
+    .encode(
+        x=alt.X("date", title=""),
+        y=alt.Y("resting_heart_rate"),
+        color=alt.Color(
+            "rolling:N",
+            title="",
+            scale=alt.Scale(
+                range=[
+                    st.secrets["theme"]["primaryColor"],
+                    st.secrets["theme"]["secondaryColor"],
+                ]
+            ),
+            legend=alt.Legend(
+                orient="top",
+            ),
+        ),
+    )
+)
+st.altair_chart(
+    rhr_scatterplot + rolling_mean_chart, use_container_width=True, theme="streamlit"
+)
 
 # Density plot
-rhr_histogram = alt.Chart(rhr_df).mark_bar(color='#3B97F3').encode(
-    x=alt.X(
-        'resting_heart_rate:N',
-        title='Resting Heart Rate'
-        # title='Resting Heart Rate',
-    ),
-    y=alt.Y('count()', title='Count')
+rhr_histogram = (
+    alt.Chart(rhr_df)
+    .mark_bar(color="#3B97F3")
+    .encode(
+        x=alt.X(
+            "resting_heart_rate:N",
+            title="Resting Heart Rate"
+            # title='Resting Heart Rate',
+        ),
+        y=alt.Y("count()", title="Count"),
+    )
 )
-st.altair_chart(rhr_histogram, use_container_width=True, theme='streamlit')
+st.altair_chart(rhr_histogram, use_container_width=True, theme="streamlit")
 
 # st.header('Heart Rate')
 # hr_df = get_garmin_data("""SELECT date, hr FROM heart_rate WHERE hr > 0""").copy()
@@ -125,8 +137,8 @@ st.altair_chart(rhr_histogram, use_container_width=True, theme='streamlit')
 #     )
 # )
 
-# percentile_hr = hr_df.groupby(hr_df.date.dt.date).hr.quantile(q=np.arange(0, 1.1, .1)).unstack()
-# percentile_hr.columns = [f"{decile*100:.0f}-{decile*100+10:.0f}%" for decile in percentile_hr.columns]
+# percentile_hr = hr_df.groupby(hr_df.date.dt.date).hr.quantile(q=np.arange(0, 1.1, .1)).unstack()  # noqa: E501
+# percentile_hr.columns = [f"{decile*100:.0f}-{decile*100+10:.0f}%" for decile in percentile_hr.columns]  # noqa: E501
 
 # percentile_hr = percentile_hr.reset_index()
 # palette = sns.color_palette("vlag", 10).as_hex()
