2122025fa3abd0124a601b4df0ba994c6b9e34d3
Hugo Le Moine <hugo.le-moine@outlook.fr>
2024-02-13 08:41:38 +0100

    PEP8

--- Quantified_Self.py
+++ Quantified_Self.py
@@ -1,38 +1,62 @@
 import streamlit as st
-import pandas as pd
 
 st.set_page_config(
     page_title="Quantified Self",
     #  layout="wide",
     #  initial_sidebar_state="expanded",
-     menu_items={
-         'Get help': 'https://twitter.com/hugo_le_moine_',
-         'Report a Bug': 'https://twitter.com/hugo_le_moine_',
-         'About': "# Quantified Self. Personal data collection and reporting."
-     }
- )
+    menu_items={
+        "Get help": "https://twitter.com/hugo_le_moine_",
+        "Report a Bug": "https://twitter.com/hugo_le_moine_",
+        "About": "# Quantified Self. Personal data collection and reporting.",
+    },
+)
 
-st.title('Quantified Self')
-st.markdown('This project aims at collecting, storing and reporting my personal data.')
+st.title("Quantified Self")
+st.markdown("This project aims at collecting, storing and reporting my personal data.")
+
+
+page_list = {
+    "Activity": "steps history.",
+    "Anki": "AnkiDroid revision history.",
+    "Body_Stress": "stress level from Garmin",
+    "Carbon_Footprint": "CO2 emissions from transportation.",
+    "Fasting": "fasting history and analysis.",
+    "Finance": "dividends collected.",
+    "Heart_Rate": "Resting heart rate.",
+    "Podcasts": "podcast listening history and yearly tops.",
+    "Sleep": " sleep data from Garmin.",
+    "Weight": "Weight data from Garmin.",
+}
+
+st.markdown("### Available pages:")
+
+for page in page_list:
+    st.markdown(
+        (
+            f"""- <a href="/{page}" target="_self">{page.replace('_', ' ')}</a>: """
+            f"""{page_list[page]}"""
+        ),
+        unsafe_allow_html=True,
+    )
+
 
 st.markdown(
     """
     ### Available pages:
-    - <a href="/Activity" target="_self">Activity</a>: steps history.
-    - <a href="/Anki" target="_self">Anki</a>: AnkiDroid revision history.
     - <a href="/Body_Stress" target="_self">Body Stress</a>: Stress level from Garmin.
-    - <a href="/Carbon_Footprint" target="_self">CO2 emissions from transportation.
+    - <a href="/Carbon_Footprint" target="_self">Carbon Footprint</a>: CO2 emissions from transportation.
     - <a href="/Fasting" target="_self">Fasting</a>: fasting history and analysis.
     - <a href="/Finance" target="_self">Finance</a>: dividends collected.
     - <a href="/Heart_Rate" target="_self">Heart Rate</a>: resting heart rate.
     - <a href="/Podcasts" target="_self">Podcasts</a>: podcast listening history and yearly tops.
     - <a href="/Sleep" target="_self">Sleep</a>: Sleep data from Garmin.
     - <a href="/Weight" target="_self">Weight</a>: Weight data from Garmin.
-    """,
-    unsafe_allow_html=True
+    """,  # noqa: E501
+    unsafe_allow_html=True,
 )
 
-st.markdown("""
+st.markdown(
+    """
     ### Data sources include:
     - AnkiDroid: an app to learn vocabulary through flashcards.
     - Garmin: biometric data collected through my watch.
@@ -42,5 +66,5 @@ st.markdown("""
     - Spotify: listening history.
     - Zero : an intermittent fasting tracker.
     """,
-    unsafe_allow_html=True
-    )
+    unsafe_allow_html=True,
+)
